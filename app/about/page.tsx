fedef27ac5a9cdb89b4db3b1b4cc9ed1447b33c6
Amaan <amaanshafie27317@gmail.com>
2024-09-29 12:29:04 +0530

    Hello

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,11 +1,12 @@
 import { BlogPosts } from "app/components/posts";
 import Image from "next/image";
-
+import img from 'public/pro pic.jpg'
 export default function about() {
+
   return (
     <section>
-      <div className="flex gap-x-4 items-center flex-row mb-10">
-     
+      <div classNames="flex gap-x-4 items-center flex-row mb-10">
+     <Image src={img} alt='img'/>
         <div>
           <p className="text-xl font-semibold">About Doe</p>
           <p className="text-lg text-gray-400">Student at SLIIT</p>
