769f74a2799795bcb64259be9d120beba4a3dcae
Amaan <amaanshafie27317@gmail.com>
2024-09-29 12:13:08 +0530

    Ola

---
+++ app/meetup/page.tsx
@@ -0,0 +1,22 @@
+import { BlogPosts } from "app/components/posts";
+import Image from "next/image";
+
+export default function meetup() {
+  return (
+    <section>
+      <div className="flex gap-x-4 items-center flex-row mb-10">
+     
+        <div>
+          <p className="text-xl font-semibold">404 page not found 🚀</p>
+          <p className="text-lg text-gray-400">Student</p>
+        </div>
+      </div>
+      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
+        Welcome! 👋
+      </h1>
+      <p className="mb-4">
+       Click 
+      </p>
+    </section>
+  );
+}
